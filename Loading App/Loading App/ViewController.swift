9974416f15a6a5d93574ea4d1026d85fe5d14489
Jordan Christensen <jordan.c4922@gmail.com>
2019-10-17 05:12:39 +0800

    Add framework

---
+++ Loading App/Loading App/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  Loading App
+//
+//  Created by Jordan Christensen on 10/17/19.
+//  Copyright © 2019 Mazjap Co Technologies. All rights reserved.
+//
+
+import UIKit
+import Loading_UI
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Loading UI/Loading UI/LoadingUI.swift
@@ -0,0 +1,9 @@
+//
+//  LoadingUI.swift
+//  Loading UI
+//
+//  Created by Jordan Christensen on 10/17/19.
+//  Copyright © 2019 Mazjap Co Technologies. All rights reserved.
+//
+
+import Foundation
