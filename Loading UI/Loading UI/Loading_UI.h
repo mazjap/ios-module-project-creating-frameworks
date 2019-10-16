f007e900f83e191928642b3adfe79999e124e638
Jordan Christensen <jordan.c4922@gmail.com>
2019-10-17 05:06:56 +0800

    Add xcode project

---
+++ Loading UI/Loading UI/Loading_UI.h
@@ -0,0 +1,19 @@
+//
+//  Loading_UI.h
+//  Loading UI
+//
+//  Created by Jordan Christensen on 10/17/19.
+//  Copyright © 2019 Mazjap Co Technologies. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for Loading_UI.
+FOUNDATION_EXPORT double Loading_UIVersionNumber;
+
+//! Project version string for Loading_UI.
+FOUNDATION_EXPORT const unsigned char Loading_UIVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Loading_UI/PublicHeader.h>
+
+
